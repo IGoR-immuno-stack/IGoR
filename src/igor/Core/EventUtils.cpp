54d176f921bee469278b7f49985c12075a66b38b
Julien Wintz <julien.wintz@inria.fr>
2025-11-26 15:59:06 +0100

    Refactor Rec_Event hierarchy to reduce code duplication
    Introduced `EventUtils` to centralize common logic for gene choice checking, sequence construction, and insertion length retrieval. Refactored Gene_choice, Deletion, Insertion, Dinuclmarkov, Pgencounter, and Hypermutationglobalerrorrate to use these utilities and a new iterate_common method in the Rec_Event base class.
    
    Key changes:
    - Added `EventUtils` namespace with check_gene_choice, build_scenario_sequence, and get_insertion_len_max.
    - Lifted iterate_common logic to Rec_Event base class.
    - Refactored event classes to remove duplicated code.
    - Added unit tests for `EventUtils` in tst/test_EventUtils.cpp.
    - Updated `Int_Str` to support testing.

---
+++ src/igor/Core/EventUtils.cpp
@@ -0,0 +1,85 @@
+#include <igor/Core/EventUtils.h>
+#include <igor/Core/Rec_Event.h>
+
+namespace EventUtils {
+
+GeneChoiceStatus check_gene_choice(
+    Gene_class gene,
+    const std::unordered_map<std::tuple<Event_type, Gene_class, Seq_side>,
+                             std::shared_ptr<Rec_Event>> &events_map,
+    const std::unordered_set<Rec_Event_name> &processed_events) {
+
+  GeneChoiceStatus status;
+  status.exists = false;
+  status.chosen = false;
+  status.event_ptr = nullptr;
+
+  auto key = std::make_tuple(GeneChoice_t, gene, Undefined_side);
+  if (events_map.count(key) != 0) {
+    status.exists = true;
+    status.event_ptr = events_map.at(key);
+    if (processed_events.count(status.event_ptr->get_name()) != 0) {
+      status.chosen = true;
+    }
+  }
+  return status;
+}
+
+Int_Str build_scenario_sequence(const Seq_type_str_p_map &constructed_sequences,
+                                bool has_v, bool has_d, bool has_j,
+                                bool has_vd_ins, bool has_dj_ins,
+                                bool has_vj_ins) {
+
+  Int_Str scenario_resulting_sequence;
+
+  if (has_v) {
+    scenario_resulting_sequence += (*constructed_sequences.at(V_gene_seq));
+  }
+
+  if (has_d) {
+    if (has_vd_ins) {
+      scenario_resulting_sequence += (*constructed_sequences.at(VD_ins_seq));
+    }
+    scenario_resulting_sequence += (*constructed_sequences.at(D_gene_seq));
+    if (has_dj_ins) {
+      scenario_resulting_sequence += (*constructed_sequences.at(DJ_ins_seq));
+    }
+  } else {
+    if (has_vj_ins) {
+      scenario_resulting_sequence += (*constructed_sequences.at(VJ_ins_seq));
+    }
+  }
+
+  if (has_j) {
+    scenario_resulting_sequence += (*constructed_sequences.at(J_gene_seq));
+  }
+
+  return scenario_resulting_sequence;
+}
+
+int get_insertion_len_max(
+    Gene_class gene_pair,
+    const std::unordered_map<std::tuple<Event_type, Gene_class, Seq_side>,
+                             std::shared_ptr<Rec_Event>> &events_map) {
+  auto key = std::make_tuple(Insertion_t, gene_pair, Undefined_side);
+  if (events_map.count(key) != 0) {
+    return events_map.at(key)->get_len_max();
+  }
+  return 0;
+}
+
+void initialize_offset_memory(
+    const std::vector<std::pair<std::shared_ptr<const Rec_Event>, int>>
+        &offset_vector,
+    Index_map &index_map,
+    std::forward_list<std::tuple<int, int, int>> &memory_and_offsets) {
+
+  for (auto iter = offset_vector.begin(); iter != offset_vector.end(); ++iter) {
+    int event_identitfier = (*iter).first->get_event_identifier();
+    index_map.request_memory_layer(event_identitfier);
+    memory_and_offsets.emplace_front(
+        event_identitfier,
+        index_map.get_current_memory_layer(event_identitfier), (*iter).second);
+  }
+}
+} // namespace EventUtils
--- src/igor/Core/IntStr.h
+++ src/igor/Core/IntStr.h
@@ -5,7 +5,8 @@
  *      Author: Quentin Marcou
  *
  *  This source code is distributed as part of the IGoR software.
- *  IGoR (Inference and Generation of Repertoires) is a versatile software to analyze and model immune receptors
+ *  IGoR (Inference and Generation of Repertoires) is a versatile software to
+ analyze and model immune receptors
  *  generation, selection, mutation and all other processes.
  *   Copyright (C) 2017  Quentin Marcou
  *
@@ -26,93 +27,95 @@
 
 #pragma once
 
-#include <vector>
 #include <ostream>
+#include <vector>
 
-class Int_Str: public std::vector<int> {
+class Int_Str : public std::vector<int> {
 
 public:
+  using std::vector<int>::vector;
+  static const std::size_t npos = -1;
 
-	static const std::size_t npos = -1;
+  Int_Str &operator+=(const Int_Str &);
+  Int_Str &operator+=(const int &);
+  Int_Str &operator+=(int &&);
+  // Int_Str& operator+=(int);
+  Int_Str &append(const Int_Str &);
+  Int_Str &append(const int &);
 
-	Int_Str& operator+=(const Int_Str&);
-	Int_Str& operator+=(const int&);
-	Int_Str& operator+=( int&& );
-	//Int_Str& operator+=(int);
-	Int_Str& append(const Int_Str&);
-	Int_Str& append(const int&);
+  Int_Str operator+(const Int_Str &) const;
+  Int_Str operator+(const int &) const;
+  Int_Str operator+(int) const;
 
-	Int_Str operator+(const Int_Str&) const;
-	Int_Str operator+(const int&) const;
-	Int_Str operator+(int) const;
+  bool operator==(const Int_Str &other) const {
+    return static_cast<const std::vector<int> &>(*this) ==
+           static_cast<const std::vector<int> &>(other);
+  }
 
-	Int_Str substr (std::size_t pos = 0, std::size_t len = npos ) const; //TODO correct this with aproper value
+  Int_Str
+  substr(std::size_t pos = 0,
+         std::size_t len = npos) const; // TODO correct this with aproper value
 
-	using std::vector<int>::erase;
-	Int_Str& erase(std::size_t pos , std::size_t len );
+  using std::vector<int>::erase;
+  Int_Str &erase(std::size_t pos, std::size_t len);
 
+  /*	Int_Str();
+          //Int_Str (const std::string& str);//cannot be unambiguously
+     identified Int_Str (const Int_Str&);
+          //string (const string& str, size_t pos, size_t len =
+     npos);//substring (3)
+          //string (const char* s);//from c-string (4)
+          //string (const char* s, size_t n);//from buffer (5)
+          //string (size_t n, char c);//fill (6)
+          //template <class InputIterator> string  (InputIterator first,
+     InputIterator last);//range (7)
+          //string (initializer_list<char> il);//initializer list (8)
+          //string (string&& str) noexcept;//move (9)
+          virtual ~Int_Str();
+          Int_Str& operator=(const Int_Str&);*/
 
+  // Iterators
 
-/*	Int_Str();
-	//Int_Str (const std::string& str);//cannot be unambiguously identified
-	Int_Str (const Int_Str&);
-	//string (const string& str, size_t pos, size_t len = npos);//substring (3)
-	//string (const char* s);//from c-string (4)
-	//string (const char* s, size_t n);//from buffer (5)
-	//string (size_t n, char c);//fill (6)
-	//template <class InputIterator> string  (InputIterator first, InputIterator last);//range (7)
-	//string (initializer_list<char> il);//initializer list (8)
-	//string (string&& str) noexcept;//move (9)
-	virtual ~Int_Str();
-	Int_Str& operator=(const Int_Str&);*/
+  /*
+          //Capacity
+          size_t max_size() const noexcept;
+          //void resize();
+          size_t capacity() const noexcept;
+          void clear() noexcept;
+          bool empty() const noexcept;
+          size_t size() const noexcept;
 
-	//Iterators
+          //Element access
+          const int& operator[]() const;
+          int& operator[]();
+          const int& at() const;
+          int& at() const;
+          int& front();
+          int& back();
+  */
 
+  // erase()
 
-/*
-	//Capacity
-	size_t max_size() const noexcept;
-	//void resize();
-	size_t capacity() const noexcept;
-	void clear() noexcept;
-	bool empty() const noexcept;
-	size_t size() const noexcept;
+  // String operations
+  // size_t find();
+  // copy()
+  // substr()
+  // compare()
 
-	//Element access
-	const int& operator[]() const;
-	int& operator[]();
-	const int& at() const;
-	int& at() const;
-	int& front();
-	int& back();
-*/
-
-
-	//erase()
-
-	//String operations
-	//size_t find();
-	//copy()
-	//substr()
-	//compare()
-
-/*private:
-	std::vector<int> int_vector;*/
-
+  /*private:
+          std::vector<int> int_vector;*/
 };
 
-namespace std{
+namespace std {
 
-template<>
-	 struct hash<Int_Str>{
-		std::size_t operator()(Int_Str const& int_str) const {
-		  std::size_t seed = int_str.size();
-		  for(auto& i : int_str) {
-			seed ^= i + 0x9e3779b9 + (seed << 6) + (seed >> 2);
-		  }
-		  return seed;
-		}
-	 };
+template <> struct hash<Int_Str> {
+  std::size_t operator()(Int_Str const &int_str) const {
+    std::size_t seed = int_str.size();
+    for (auto &i : int_str) {
+      seed ^= i + 0x9e3779b9 + (seed << 6) + (seed >> 2);
+    }
+    return seed;
+  }
+};
 
-
-}
+} // namespace std
